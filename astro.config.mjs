29c642ca5380bd0ea64de40d5cc725ce7763d258
byron <byrondalberg@gmail.com>
2022-09-02 15:19:01 -0700

    new commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://BihonAnon.github.io',
+  base: '/brothers-homewood-management',
   integrations: [tailwind()]
 });
